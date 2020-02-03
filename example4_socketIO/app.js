4b0f70634fb56b11b8b857a0df0cd69ccd93e2fc
luckystar360 <ngocnhat95@gmail.com>
2020-02-03 16:52:16 +0700

    example4

--- example4_socketIO/app.js
+++ example4_socketIO/app.js
@@ -23,7 +23,10 @@ io.on("connection",function(socket){
     });
     socket.on("chat message",function(msg){
         console.log(socket.id + ": " + msg)
-        socket.broadcast.emit("return message",msg,socket.id);
+        //only send to other client
+        io.broadcast.emit("return message",msg,socket.id);
+        //send to all client
+        //io.sockets.emit("return message",msg,socket.id);
     });
 });
 
